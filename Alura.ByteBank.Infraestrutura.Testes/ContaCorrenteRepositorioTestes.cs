3b3265583b3afcf6a9fc58e684da3bac550684cf
Marcelo <marceloinoue@hotmail.com>
2023-02-14 15:23:52 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
@@ -0,0 +1,167 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Alura.ByteBank.Infraestrutura.Testes.Servico;
+using Alura.ByteBank.Infraestrutura.Testes.Servico.DTO;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ContaCorrenteRepositorioTestes
+    {
+        private readonly IContaCorrenteRepositorio _repositorio;
+        public ITestOutputHelper SaidaConsoleTeste { get; set; }
+
+        public ContaCorrenteRepositorioTestes(ITestOutputHelper _saidaConsoleTeste)
+        {
+            SaidaConsoleTeste = _saidaConsoleTeste;
+            SaidaConsoleTeste.WriteLine("Construtor invocado.");
+
+            //Injetando dependências no construtor;
+            var servico = new ServiceCollection();
+            servico.AddTransient<IContaCorrenteRepositorio, ContaCorrenteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _repositorio = provedor.GetService<IContaCorrenteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodasContasCorrentes()
+        {
+            //Arrange
+            //Act
+            List<ContaCorrente> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(2, lista.Count);
+        }
+
+        [Fact]
+        public void TestaObterContaCorrentePorId()
+        {
+            //Arrange
+            //Act
+            var conta = _repositorio.ObterPorId(1);
+
+            //Assert
+            Assert.NotNull(conta);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void TestaObterContaCorrentePorVariosId(int id)
+        {
+            //Arrange
+            //Act
+            var conta = _repositorio.ObterPorId(id);
+
+            //Assert
+            Assert.NotNull(conta);
+        }
+
+        [Fact]
+        public void TestaAtualizaSaldoDeterminadaConta()
+        {
+            //Arrange
+            var conta = _repositorio.ObterPorId(1);
+            double saldoNovo = 15;
+            conta.Saldo = saldoNovo;
+
+            //Act
+            var atualizado = _repositorio.Atualizar(1, conta);
+
+            //Assert
+            Assert.True(atualizado);
+        }
+
+        [Fact]
+        public void TestaInserirUmaNovaContaCorrenteNoBancoDeDados()
+        {
+            //Arrange
+            var conta = new ContaCorrente()
+            {
+                Saldo = 10,
+                Identificador = Guid.NewGuid(),
+                Cliente = new Cliente()
+                {
+                    Nome = "Kent Nelson",
+                    CPF = "486.074.980-45",
+                    Identificador = Guid.NewGuid(),
+                    Profissao = "Bancário",
+                    Id = 1
+                },
+                Agencia = new Agencia()
+                {
+                    Nome = "Agencia Central Coast City",
+                    Identificador = Guid.NewGuid(),
+                    Id = 1,
+                    Endereco = "Rua das Flores, 25",
+                    Numero = 147
+                }
+            };
+
+            //Act
+            var retorno = _repositorio.Adicionar(conta);
+
+            //Assert
+            Assert.True(retorno);
+        }
+
+        // Testes com Mock
+        [Fact]
+        public void TestaObterContasMock()
+        {
+            //Arange
+            var bytebankRepositorioMock = new Mock<IByteBankRepositorio>();
+            var mock = bytebankRepositorioMock.Object;
+
+            //Act
+            var lista = mock.BuscarContasCorrentes();
+
+            //Assert - Verificando o comportamento
+            bytebankRepositorioMock.Verify(b => b.BuscarContasCorrentes());
+        }
+
+        [Fact]
+        public void TestaConsultaPix()
+        {
+            //Arange
+            var guid = new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a");
+            var pix = new PixDTO() { Chave = guid, Saldo = 10 };
+
+            var pixRepositorioMock = new Mock<IPixRepositorio>();
+            pixRepositorioMock.Setup(x => x.consultaPix(It.IsAny<Guid>())).Returns(pix);
+
+            var mock = pixRepositorioMock.Object;
+
+            //Act
+            var saldo = mock.consultaPix(guid).Saldo;
+
+            //Assert
+            Assert.Equal(10, saldo);
+        }
+
+        [Fact]
+        public void TestaConsultaPixPorChaveMock()
+        {
+            //Arange
+            var pixRepositorioMock = new Mock<IPixRepositorio>();
+            var mock = pixRepositorioMock.Object;
+
+            //Act
+            var lista = mock.consultaPix(new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a"));
+
+            //Assert - Verificando o comportamento
+            pixRepositorioMock.Verify(b => b.consultaPix(new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a")));
+        }
+    }
+}
